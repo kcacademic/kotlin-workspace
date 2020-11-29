b2d5fc8734823eae39a19a8c506d3bc33b7772c4
CHANDRAKANT Kumar <kumar.chandrakant@soprabanking.com>
2020-11-30 02:18:17 +0530

    Adding daily batch of work.

---
+++ kotlin-multiplatform/src/commonMain/kotlin/org/jetbrains/base64/Base64.kt
@@ -0,0 +1,13 @@
+package org.jetbrains.base64
+
+interface Base64Encoder {
+    fun encode(src: ByteArray): ByteArray
+
+    fun encodeToString(src: ByteArray): String {
+        val encoded = encode(src)
+        return buildString(encoded.size) {
+            encoded.forEach { append(it.toChar()) }
+        }
+    }
+
+}
